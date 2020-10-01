ffa651ac0a8609a38baf0a786475169f5dda5173
Chad Kruszynsky <ckruszynsky@turner-industries.com>
2020-10-01 12:55:07 -0500

    add certs and https ports

--- CoffeeAPI/Startup.cs
+++ CoffeeAPI/Startup.cs
@@ -36,7 +36,7 @@ namespace CoffeeAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+           // app.UseHttpsRedirection();
 
             app.UseRouting();
 
--- TeaAPI/Startup.cs
+++ TeaAPI/Startup.cs
@@ -36,7 +36,7 @@ namespace TeaAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
